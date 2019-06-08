3299385ab394f99d3f35f8b3add5b56cf1393a45
Fran Mosteiro <fmosteiro@theinit.com>
2019-06-08 14:35:08 +0200

    Dirty apple little secrets2

--- resources/keystore/applestore/insert-sueño-faltante.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `health_ldsalud`.`game_user_event` (`date_created`, `event_id`, `magnitude`, `points`, `user_id`, `source_id`) VALUES ( '2019-06-05 00:00:00', '14', '7', '70', '34145', '18051');
-INSERT INTO `health_ldsalud`.`game_user_event` ( `date_created`, `event_id`, `magnitude`, `points`, `user_id`, `source_id`) VALUES ( '2019-06-05 00:00:00', '14', '7', '70', '34498', '18051');
-INSERT INTO `health_ldsalud`.`game_user_event` ( `date_created`, `event_id`, `magnitude`, `points`, `user_id`, `source_id`) VALUES ( '2019-06-06 00:00:00', '14', '7', '70', '1003172', '18052');
-INSERT INTO `health_ldsalud`.`game_user_event` ( `date_created`, `event_id`, `magnitude`, `points`, `user_id`, `source_id`) VALUES ( '2019-06-06 00:00:00', '14', '8', '70', '1004897', '18052');
-INSERT INTO `health_ldsalud`.`game_user_event` ( `date_created`, `event_id`, `magnitude`, `points`, `user_id`, `source_id`) VALUES ( '2019-06-05 00:00:00', '14', '8', '70', '1005176', '18051');
-INSERT INTO `health_ldsalud`.`game_user_event` ( `date_created`, `event_id`, `magnitude`, `points`, `user_id`, `source_id`) VALUES ( '2019-06-05 00:00:00', '14', '7', '70', '1011073', '18051');
