4f4cbce96a857fa1a34e6273c6e71bf387aa9748
Héctor <hvillar@theinit.com>
2019-06-20 14:31:03 +0200

    Initial vue migration

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,3 @@
-module.exports = api => {
-  const isProd = api.cache(() => process.env.NODE_ENV === 'production')
-
-  return {
-    plugins: [
-      '@babel/plugin-syntax-dynamic-import'
-    ],
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          modules: false,
-          targets: {
-            esmodules: !isProd
-          }
-        }
-      ]
-    ]
-  }
-}
+module.exports = {
+  presets: ["@vue/app"]
+};
